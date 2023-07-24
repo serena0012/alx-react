16d6e4408a7c25e76b8851b2dc1aebaf4e0c27bf
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-07-24 15:45:50 -0400

    updated

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,18 @@
-// task_1/js/dashboard_main.js
-import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import $ from "jquery";
+import _ from 'lodash';
 
-const updateCounter = debounce(() => {
-  const count = parseInt($('#count').text()) || 0;
-  $('#count').text(`${count + 1} clicks on the button`);
-}, 1000);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
-$(document).ready(() => {
-  const root = $('#root');
 
-  root.append('<p>Holberton Dashboard</p>');
-  root.append('<p>Dashboard data for the students</p>');
-  root.append('<button id="clickButton">Click here to get started</button>');
-  root.append('<p id="count"></p>');
-  root.append('<p>Copyright - Holberton School</p>');
+let count = 0;
 
-  $('#clickButton').on('click', updateCounter);
-});
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,28 +1,34 @@
-// task_2/webpack.config.js
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    mode: 'production',
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    performance: {
+    maxAssetSize: 1000000,
   },
-  mode: 'production',
-  module: {
+    module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'images',
-        },
-      },
-    ],
-  },
-};
-
+        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      }
+    ]
+  }
+}
