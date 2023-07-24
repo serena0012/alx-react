54ad98eafc27d8afdde192f415d15f647dd2361d
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-07-24 12:26:17 -0400

    Task 1 added

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,11 @@
+// task_1/webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production', // Set the mode to 'production'
+};
