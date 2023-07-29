ec704a588f3866feb376d008deb73586af2ab263
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-07-29 16:14:52 -0400

    Task 4 updated

---
+++ 0x01-react_intro/task_4/dashboard/build/precache-manifest.70a9c94e98a0741cde16baaea2309357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d94d0d6e3f33002bebace19cb5c35c8",
+    "url": "/holbertonschool-web_react/index.html"
+  },
+  {
+    "revision": "d59449ae06c934952c98",
+    "url": "/holbertonschool-web_react/static/css/main.6b9b2d71.chunk.css"
+  },
+  {
+    "revision": "d039c460645b4b884415",
+    "url": "/holbertonschool-web_react/static/js/2.152c7587.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/holbertonschool-web_react/static/js/2.152c7587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5261ed082016dbbbda",
+    "url": "/holbertonschool-web_react/static/js/3.113019f0.chunk.js"
+  },
+  {
+    "revision": "d59449ae06c934952c98",
+    "url": "/holbertonschool-web_react/static/js/main.495e8563.chunk.js"
+  },
+  {
+    "revision": "e21de18bc9ead5daaf75",
+    "url": "/holbertonschool-web_react/static/js/runtime-main.dc8f8899.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/holbertonschool-web_react/static/media/HS_logo.23d20eb3.jpg"
+  }
+]);
